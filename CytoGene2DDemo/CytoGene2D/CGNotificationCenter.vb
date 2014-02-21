d6a7893a04d42b1d8028e7e5830f2d503d89c2c0
Yifan Cai <yifancai@Yifans-iMac.local>
2014-02-20 18:17:01 -0800

    notification center...

--- CytoGene2DDemo/CytoGene2D/CGNotificationCenter.vb
+++ CytoGene2DDemo/CytoGene2D/CGNotificationCenter.vb
@@ -13,7 +13,7 @@ Public Class CGNotificationCenter
 	Private Shared sharedInstance_ As CGNotificationCenter
 	Private Shared lock_ As New Object
 
-	Private notifications As HashSet(Of Notification)
+	Private notifications As Hashtable
 
 	Private Class Notification 
 		Private name As String
@@ -28,6 +28,10 @@ Public Class CGNotificationCenter
 			Return name
 		End Function
 
+		Public Function getObservers() As ArrayList
+			Return observers
+		End Function
+
 		Public Sub addObserver(ByVal observer As IObserver)
 			observers.Add(observer)
 		End	Sub
@@ -39,25 +43,33 @@ Public Class CGNotificationCenter
 		Public Sub removeAllObservers()
 			observers.Clear()
 		End Sub
+
+		Public Function containObserver(ByVal observer As IObserver) As Boolean
+			Return observers.Contains(observer)
+		End Sub
+
+		Public Function isEmpty() As Boolean 'no one is listening'
+			Return observers.Count == 0
+		End Sub
 	End Class
 
 	' VBisshitdesignedlanguage '
-	Private Class NotificationEqualityComparer Implements IEqualityComparer (Of Notification)
-		Public Overloads Function Equals(ByVal n1 As Notification, ByVal n2 As Notification) _
-	                   As Boolean Implements IEqualityComparer(Of Notification).Equals
-	        ' same name but different objects'           
-	        If n1.getName() = n2.getName() And n1 != n2 Then 
-	            Return True 
-	        Else 
-	            Return False 
-	        End If 
-    	End Function 
-
-	    Public Overloads Function GetHashCode(ByVal n As Notification) _
-	                As Integer Implements IEqualityComparer(Of Notification).GetHashCode
-	        Return n.getName().GetHashCode()
-	    End Function 
-	End Class
+'	Private Class NotificationEqualityComparer Implements IEqualityComparer (Of Notification)
+'		Public Overloads Function Equals(ByVal n1 As Notification, ByVal n2 As Notification) _
+'	                   As Boolean Implements IEqualityComparer(Of Notification).Equals
+'	        ' same name but different objects'           
+'	        If n1.getName() = n2.getName() And n1 != n2 Then 
+'	            Return True 
+'	        Else 
+'	            Return False 
+'	        End If 
+'    	End Function 
+'
+'	    Public Overloads Function GetHashCode(ByVal n As Notification) _
+'	                As Integer Implements IEqualityComparer(Of Notification).GetHashCode
+'	        Return n.getName().GetHashCode()
+'	    End Function 
+'	End Class
 
 	Public Shared Function sharedNotificationCenter() As CGNotificationCenter
 		If sharedInstance_ Is Nothing Then
@@ -70,22 +82,44 @@ Public Class CGNotificationCenter
 	End Function
 
 	Protected Sub New()
-		notifications = New HashSet(Of Notification)(New NotificationEqualityComparer())
+		notifications = New Hashtable
 	End Sub
 
 	Public Sub registerObserverForNotification(ByVal observer As IObserver, ByVal notificationName As String)
-
+		If notifications.ContainsKey(notificationName) Then
+			Dim n As Notification = notifications.Item(notificationName)
+			n.addObserver(observer)
+		Else
+			Dim n As New Notification(notificationName)
+			n.addObserver(observer)
+			notifications.Add(notificationName, n)
+		End If
 	End Sub
 
-	Public Sub unregisterObserverForNotification(ByVal obj As Object, ByVal notificationName As String)
-
+	Public Sub unregisterObserverForNotification(ByVal observer As IObserver, ByVal notificationName As String)
+		If notifications.ContainsKey(notificationName) Then
+			Dim n As Notification = notifications.Item(notificationName)
+			n.removeObserver(observer)
+		End If
 	End Sub
 
-	Public Sub unregisterObserverForAllNotification(ByVal obj As Object)
-
+	Public Sub unregisterObserverForAllNotification(ByVal observer As IObserver)
+		Dim vNotifs As ICollection = notifications.Values
+		Dim vNotifArray(notifications.Count - 1) As Notification
+		vNotifs.CopyTo(vNotifArray, 0)
+		For Each n As Notification in vNotifArray
+			If n.containObserver(observer) Then
+				n.removeObserver(observer)
+				If n.isEmpty() Then ' remove the notification if no one is listening to it'
+					notifications.Remove(n.getName())
+				End If
+			End If
+		Next
 	End Sub
 
-	Public Sub postNotification(ByVal notificationName As String, ByVal info As Object)
-
+	Public Sub postNotification(ByVal sender As Object, ByVal notificationName As String, ByVal info As Object)
+		Dim n As Notification = notifications.Item(notificationName)
+		Dim observers As ArrayList = n.getObservers()
+		'todo'
 	End Sub
 End Class
